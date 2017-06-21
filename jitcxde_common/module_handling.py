a5c761607324a235c2e682a4c431951db7af939b
Gerrit Ansmann <bla@example.com>
2017-06-21 17:54:42 +0200

    Forgot to include a file

---
+++ jitcxde_common/module_handling.py
@@ -0,0 +1,11 @@
+from sys import version_info
+
+if version_info < (3,):
+	from .module_handling27 import get_module_path, modulename_from_path, find_and_load_module, module_from_path
+elif (3,) <= version_info < (3,3):
+	raise NotImplementedError("Module loading for Python versions between 3 and 3.3 was not implemented. Please upgrade to a newer Python version.")
+elif (3,3) <= version_info < (3,5):
+	from .module_handling33 import get_module_path, modulename_from_path, find_and_load_module, module_from_path
+elif (3,5) <= version_info:
+	from .module_handling35 import get_module_path, modulename_from_path, find_and_load_module, module_from_path
+
