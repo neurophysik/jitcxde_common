12e8b4214c05300917bbeecb63709367d128f57f
Gerrit Ansmann <bla@example.com>
2017-10-20 12:58:28 +0200

    Code test, rough version

---
+++ tests/test_code.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sympy
+import numpy
+from jitcxde_common import jitcxde,handle_input,render_and_write_code
+from numpy.testing import assert_allclose
+
+class jitcxde_tester(jitcxde):
+	def __init__(self, f_sym=(), n=None, verbose=True, module_location=None):
+		jitcxde.__init__(self,verbose,module_location)
+		self.f_sym, self.n = handle_input(f_sym,n)
+	
+	def _default_arguments(self):
+		basics = [
+			("Y", "PyArrayObject *__restrict const")
+			]
+		return basics
+	
+	def generate_f_C(self, chunk_size=100):
+		f_sym_wc = (entry for entry in self.f_sym())
+		
+		arguments = self._default_arguments()
+		
+		set_dy = sympy.Function("set_dy")
+		render_and_write_code(
+			(set_dy(i,entry) for i,entry in enumerate(f_sym_wc)),
+			self._tmpfile,
+			"f",
+			["set_dy", "y"],
+			chunk_size = chunk_size,
+			arguments = arguments+[("dY", "PyArrayObject *__restrict const")]
+			)
+	
+	def _compile_C(self):
+		if self.jitced is None:
+			self.compile_C()
+	
+	def compile_C(
+		self,
+		extra_compile_args = None,
+		extra_link_args = None,
+		verbose = False,
+		modulename = None
+		):
+		
+		self._process_modulename(modulename)
+		self._render_template(n = self.n)
+		self._compile_and_load(verbose,extra_compile_args,extra_link_args)
+
+y = sympy.Function("y")
+f = [
+		y(0),
+		2*y(1)**3,
+		y(0)+y(1)+y(2),
+		sympy.exp(y(3)),
+		5,
+	]
+
+def f_control(y):
+	return [
+		y[0],
+		2*y[1]**3,
+		y[0]+y[1]+y[2],
+		numpy.exp(y[3]),
+		5,
+	]
+
+import os
+from jitcxde_common import add_suffix
+import platform
+import shutil
+import unittest
+from tempfile import mkdtemp
+
+name = ""
+def get_unique_name():
+	global name
+	name += "x"
+	return name
+
+class basic_test(unittest.TestCase):
+	def tmpfile(self, filename):
+		return os.path.join(self.directory, filename)
+	
+	@classmethod
+	def setUpClass(self):
+		self.argdict = {"f_sym": f}
+		
+	def setUp(self):
+		self.directory = mkdtemp()
+	
+	def test_default(self):
+		self.tester = jitcxde_tester(**self.argdict)
+		self.tester.generate_f_C()
+		self.tester.compile_C()
+	
+	def test_heavily_chunked(self):
+		self.tester = jitcxde_tester(**self.argdict)
+		self.tester.generate_f_C(chunk_size=1)
+		self.tester.compile_C()
+	
+	def test_save_and_load(self):
+		self.tester = jitcxde_tester(**self.argdict)
+		self.tester.generate_f_C()
+		destination = self.tester.save_compiled(overwrite=True)
+		folder, filename = os.path.split(destination)
+		shutil.move(filename,self.tmpfile(filename))
+		self.tester = jitcxde_tester(module_location=self.tmpfile(filename))
+		
+	def test_compile_save_and_load(self,default=False):
+		self.tester = jitcxde_tester(**self.argdict)
+		self.tester.generate_f_C()
+		modulename = None if default else get_unique_name()
+		self.tester.compile_C(modulename=modulename)
+		filename = self.tester.save_compiled(overwrite=True)
+		shutil.move(filename, self.tmpfile(filename))
+		self.tester = jitcxde_tester(module_location=self.tmpfile(filename))
+		
+	def test_save_with_default_name_and_load(self):
+		self.test_compile_save_and_load(True)
+	
+	def test_save_to_directory_and_load(self):
+		self.tester = jitcxde_tester(**self.argdict)
+		self.tester.generate_f_C()
+		modulename = get_unique_name()
+		self.tester.compile_C(modulename=modulename)
+		destination = self.tester.save_compiled(self.tmpfile(""), overwrite=True)
+		folder, filename = os.path.split(destination)
+		print(folder,self.tmpfile(""))
+		assert folder==os.path.dirname(self.tmpfile(""))
+		self.tester = jitcxde_tester(module_location=self.tmpfile(filename))
+	
+	def tearDown(self):
+		arg = numpy.random.random(5)
+		assert_allclose(
+				self.tester.jitced.f(arg),
+				f_control(arg)
+			)
+		
+		if platform.system() != "Windows":
+			# Windows blocks loaded module files from removal.
+			shutil.rmtree(self.directory)
+
+def f_generator():
+	for entry in f:
+		yield entry
+
+class basic_test_with_generator_function(basic_test):
+	@classmethod
+	def setUpClass(self):
+		self.argdict = {"f_sym": f_generator, "n": len(f)}
+
+if __name__ == "__main__":
+	unittest.main(buffer=True)
+
