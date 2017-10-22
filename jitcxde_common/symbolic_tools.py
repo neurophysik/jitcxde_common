80e1807b2a98fd80f6158447710aa018c7580ed9
Gerrit Ansmann <bla@example.com>
2017-10-22 14:07:12 +0200

    Added count_calls and test and support functions with functions.

--- jitcxde_common/symbolic_tools.py
+++ jitcxde_common/symbolic_tools.py
@@ -13,13 +13,34 @@ def collect_arguments(expression, function):
 		list of all arguments with which `function` is called within `expression`.
 	"""
 	
+	result = set()
+	
 	if ( expression.__class__ == FunctionSymbol
 			and expression.get_name() == function.name ):
-		return {expression.args}
-	else:
-		return set().union(*(collect_arguments(arg, function) for arg in expression.args))
+		result.add(expression.args)
+	
+	for arg in expression.args:
+		result.update( collect_arguments(arg,function) )
+	
+	return result
+
+def count_calls(expression,function):
+	"""
+	Counts the calls of `function` within expression.
+	Similar to SymPy’s count.
+	"""
+	result = 0
+	
+	if ( expression.__class__ == FunctionSymbol
+			and expression.get_name() == function.name ):
+		result += 1
+	
+	result += sum( count_calls(arg,function) for arg in expression.args )
+	
+	return result
 
 def ordered_subs(expression,substitutions):
 	for substitution in substitutions:
 		expression = expression.subs(*substitution)
 	return expression
+
--- tests/test_symbolic_tools.py
+++ tests/test_symbolic_tools.py
@@ -1,23 +1,37 @@
 import unittest
 from symengine import Function, Symbol, sin, Integer
-from jitcxde_common.symbolic_tools import collect_arguments
+from jitcxde_common.symbolic_tools import collect_arguments, count_calls
+
+f = Function("f")
+g = Function("g")
+a = Symbol("a")
 
 class TestCollectArguments(unittest.TestCase):
 	def test_complex_expression(self):
-		f = Function("f")
-		g = Function("g")
-		a = Symbol("a")
-		
 		expression = 3**f(42) + 23 - f(43,44) + f(45+a)*sin( g(f(46,47,48)+17) - g(4) )
 		
 		self.assertEqual(
-			collect_arguments(expression, f),
-			{
-				(Integer(42),),
-				(Integer(43), Integer(44)),
-				(45+a,),
-				(Integer(46), Integer(47), Integer(48))}
+				collect_arguments(expression,f),
+				{
+					(Integer(42),),
+					(Integer(43), Integer(44)),
+					(45+a,),
+					(Integer(46), Integer(47), Integer(48))
+				}
 			)
+		
+		self.assertEqual(count_calls(expression,f),4)
+	
+	def test_function_within_function(self):
+		expression = f(f(42))
+		
+		self.assertEqual(
+				collect_arguments(expression, f),
+				{ (Integer(42),) , (f(Integer(42)),) }
+			)
+		
+		self.assertEqual(count_calls(expression,f),2)
+
 
 if __name__ == "__main__":
 	unittest.main(buffer=True)
