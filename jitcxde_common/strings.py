93bc1a69b2218eb1543ef901a56d1a1818692b88
Gerrit Ansmann <bla@example.com>
2017-10-24 00:02:02 +0200

    Removing obsolete functions.

--- jitcxde_common/strings.py
+++ jitcxde_common/strings.py
@@ -1,21 +1,3 @@
-# String manipulation
-# -------------------
-
-# obsolete
-def remove_suffix(string, suffix):
-	partition = string.rpartition(suffix)
-	if partition[1] and not partition[2]:
-		return partition[0]
-	else:
-		return string
-
-# obsolete
-def ensure_suffix(string, suffix):
-	if not string.endswith(suffix):
-		return string + suffix
-	else:
-		return string
-
 def rsplit_int(s):
 	if s and s[-1].isdigit():
 		x,y = rsplit_int(s[:-1])
--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from jitcxde_common.strings import remove_suffix, ensure_suffix, count_up
+from jitcxde_common.strings import count_up
 import unittest
 
 class StringTest(unittest.TestCase):
-	def test_remove_suffix(self):
-		self.assertEqual( remove_suffix("foo.so", ".so"), "foo" )
-		self.assertEqual( remove_suffix("foo.sob", ".so"), "foo.sob" )
-		self.assertEqual( remove_suffix("foo.xy", ".so"), "foo.xy" )
-	
-	def test_ensure_suffix(self):
-		self.assertEqual( ensure_suffix("foo", ".so"), "foo.so" )
-		self.assertEqual( ensure_suffix("foo.so", ".so"), "foo.so" )
-		self.assertEqual( ensure_suffix("foo.sob", ".so"), "foo.sob.so" )
-		
 	def test_count_up(self):
 		self.assertEqual( count_up("foo"), "foo_1" )
 		self.assertEqual( count_up("foo_2"), "foo_3" )
