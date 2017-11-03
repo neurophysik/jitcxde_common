cdf42953e070fecd27342bf3ce5acb582ef1e2bf
Gerrit Ansmann <bla@example.com>
2017-11-03 14:28:23 +0100

    Reorganising tests.

--- tests/test_transversal.py
+++ tests/test_transversal.py
@@ -1,38 +1,47 @@
 import unittest
 from jitcxde_common.transversal import GroupHandler
 
-class TestFinderAndIterator(unittest.TestCase):
-	def test_ordered_groups(self):
-		groups = [ [0,1,2], [3,4,5] ]
-		G = GroupHandler(groups)
-		
-		for i in range(6):
-			self.assertIn(i,groups[G.group_from_index(i)])
-		
-		sequence = list(G.iterate(range(6)))
-		expected = [
-				  0  ,
-				(0,1),
-				(1,2),
-				  1  ,
-				(3,4),
-				(4,5)
-			]
-		self.assertSequenceEqual(sequence,expected)
+class TestOrdered(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.n = 6
+		self.groups = [ [0,1,2], [3,4,5] ]
+		self.main_indices = [0,3]
+		self.iteration = [ 0 , (0,1), (1,2), 1 , (3,4), (4,5) ]
 	
-	def test_alternating_groups(self):
-		groups = [ [0,2,4], [1,3,5] ]
-		G = GroupHandler(groups)
-		sequence = list(G.iterate(range(6)))
-		expected = [
-				  0  ,
-				  1  ,
-				(0,2),
-				(1,3),
-				(2,4),
-				(3,5)
-			]
-		self.assertSequenceEqual(sequence,expected)
+	def setUp(self):
+		self.G = GroupHandler(self.groups)
+	
+	def test_group_from_index(self):
+		for i in range(self.n):
+			self.assertIn(i,self.groups[self.G.group_from_index(i)])
+	
+	def test_iterate(self):
+		self.assertSequenceEqual(
+				list(self.G.iterate(range(self.n))),
+				self.iteration
+			)
+		
+	def test_main_indices(self):
+		self.assertSequenceEqual(
+				list(self.G.main_indices),
+				self.main_indices
+			)
+	
+	def test_extractor(self):
+		original = range(100,100+self.n)
+		extractor,extracted = self.G.extract_main(original)
+		sequence = list(extractor())
+		self.assertSequenceEqual(sequence,original)
+		for i in self.G.main_indices:
+			self.assertEqual(extracted[i],original[i])
+
+class TestAlternating(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.groups = [ [0,2,4], [1,3,5] ]
+		self.main_indices = [0,1]
+		self.iteration = [ 0 , 1 , (0,2), (1,3), (2,4), (3,5) ]
 
 class TestErrors(unittest.TestCase):
 	def test_missing_indices(self):
@@ -47,3 +56,4 @@ class TestErrors(unittest.TestCase):
 
 if __name__ == "__main__":
 	unittest.main(buffer=True)
+
